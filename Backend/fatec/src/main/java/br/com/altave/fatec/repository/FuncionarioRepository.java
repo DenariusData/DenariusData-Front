c25cb26b890201e50e0f1cc26e5aa87f3b30642c
BeatrizSantos10 <163482399+BeatrizSantos10@users.noreply.github.com>
2025-03-24 22:19:25 -0300

    feat: initialize project

--- Backend/fatec/src/main/java/br/com/altave/fatec/repository/FuncionarioRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.altave.fatec.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.altave.fatec.model.Funcionario;
-
-public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
-}
